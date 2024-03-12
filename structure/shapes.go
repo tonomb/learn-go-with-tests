ea5fdec9eb1550e95e62b662afe3e342e06f0a94
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-03-11 19:04:27 -0600

    completed structure methods and interfaces

---
+++ structure/shapes.go
@@ -0,0 +1,39 @@
+package structure
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+
+	return rectangle.Height * rectangle.Width
+}
